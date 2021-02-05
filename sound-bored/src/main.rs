c818f148a185cfc685ac633668f493c085309ccb
Dalton Maahs <dalton.maahs@yourbind.com>
2021-02-04 23:08:11 -0600

    init porting to rust

--- sound-bored/src/main.rs
+++ sound-bored/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use hound::{SampleFormat, WavReader, WavSpec, WavWriter};
+use std::fs::File;
+use std::io::{BufReader, BufWriter};
+
+type Reader = WavReader<BufReader<File>>;
+type Writer = WavWriter<BufWriter<File>>;
+
+fn main() -> Result<(), hound::Error> {
+    let spec = WavSpec {
+        channels: 2,
+        sample_rate: 44100,
+        bits_per_sample: 16,
+        sample_format: SampleFormat::Int,
+    };
+
+    let custom_audio_filename = "audio-samples/custom.wav";
+    let fuck_audio_filename = "audio-samples/fuck.wav";
+
+    // overwrite custom file with empty wav.
+    let mut writer = WavWriter::create(custom_audio_filename, spec).unwrap();
+
+    // read and append
+    //let reader = WavReader::open(fuck_audio_filename).unwrap();
+    //append_to_writer(&mut writer, reader).expect("appended");
+
+     //read and append
+    //let reader = WavReader::open("audio-samples/the.wav").unwrap();
+    //append_to_writer(&mut writer, reader).expect("appended");
+
+    Ok(())
 }
+
